fd36f4194a3d1ad7e6b7c57d1445946256d3245e
soma_arc <somaarcr@gmail.com>
2015-12-18 17:15:10 +0900

    Use original patterns (p and cross) to display objects

--- main.cpp
+++ main.cpp
@@ -14,132 +14,142 @@ ARParam arCameraParam;
 ARGL_CONTEXT_SETTINGS_REF arSettings;
 int width = 800;
 int height = 600;
-double marker_width = 80.0;
-int marker_id;
+double crossMarkerWidth = 80.0;
+double pMarkerWidth = 40.0;
+int crossMarkerId;
+int pMarkerId;
 
 bool setupCamera(char videoConfigFile[], char cameraParamFile[]) {
 
-	if (arVideoOpen(videoConfigFile) < 0) {
-		std::cout << "Camera not found \n";
-		return false;
-	}
-	if (arVideoCapStart() != 0) {
-		std::cout << "Video capture start failed \n";
-		return false;
-	}
+  if (arVideoOpen(videoConfigFile) < 0) {
+    std::cout << "Camera not found \n";
+    return false;
+  }
+  if (arVideoCapStart() != 0) {
+    std::cout << "Video capture start failed \n";
+    return false;
+  }
 
-	ARParam	wparam;
-	if (arParamLoad(cameraParamFile, 1, &wparam) < 0) {
-		std::cout << "camera parameter file load failed \n";
-		return false;
-	}
+  ARParam	wparam;
+  if (arParamLoad(cameraParamFile, 1, &wparam) < 0) {
+    std::cout << "camera parameter file load failed \n";
+    return false;
+  }
 
-	int xsize, ysize;
-	if (arVideoInqSize(&xsize, &ysize) < 0) {
-		std::cout << "get image size - error \n";
-		return false;
-	}
+  int xsize, ysize;
+  if (arVideoInqSize(&xsize, &ysize) < 0) {
+    std::cout << "get image size - error \n";
+    return false;
+  }
 
-	arParamChangeSize(&wparam, xsize, ysize, &arCameraParam);
-	arInitCparam(&arCameraParam);
+  arParamChangeSize(&wparam, xsize, ysize, &arCameraParam);
+  arInitCparam(&arCameraParam);
 
-	return true;
+  return true;
 }
 
 void mySetLight() {
-	GLfloat light_diffuse[] = { 0.9, 0.9, 0.9, 1.0 };
-	GLfloat light_specular[] = { 1.0, 1.0, 1.0, 1.0 };
-	GLfloat light_ambient[] = { 0.3, 0.3, 0.3, 0.1 };
-	GLfloat light_position[] = { 0.0, 0.0, 0.0, 1.0 };
+  GLfloat lightDiffuse[] = { 0.9, 0.9, 0.9, 1.0 };
+  GLfloat lightSpecular[] = { 1.0, 1.0, 1.0, 1.0 };
+  GLfloat lightAmbient[] = { 0.3, 0.3, 0.3, 0.1 };
+  GLfloat lightPosition[] = { 0.0, 0.0, 0.0, 1.0 };
 
-	glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
-	glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
-	glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
-	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+  glLightfv(GL_LIGHT0, GL_DIFFUSE, lightDiffuse);
+  glLightfv(GL_LIGHT0, GL_SPECULAR, lightSpecular);
+  glLightfv(GL_LIGHT0, GL_AMBIENT, lightAmbient);
+  glLightfv(GL_LIGHT0, GL_POSITION, lightPosition);
 
-	glShadeModel(GL_SMOOTH);
-	glEnable(GL_LIGHT0);
+  glShadeModel(GL_SMOOTH);
+  glEnable(GL_LIGHT0);
 }
 
 void display() {
 
-	ARUint8* image = arVideoGetImage();
-	if (image == NULL) return;
+  ARUint8* image = arVideoGetImage();
+  if (image == NULL) return;
 
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	arglDispImage(image, &arCameraParam, 1.0, arSettings);
+  arglDispImage(image, &arCameraParam, 1.0, arSettings);
 
-	double P[16];
-        arglCameraFrustumRH(&arCameraParam, 1, 1000, P);
-	glMatrixMode(GL_PROJECTION);
-	glLoadMatrixd(P);
+  double P[16];
+  arglCameraFrustumRH(&arCameraParam, 1, 1000, P);
+  glMatrixMode(GL_PROJECTION);
+  glLoadMatrixd(P);
 
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
 
-	mySetLight();
-	glEnable(GL_LIGHTING);
-	glEnable(GL_DEPTH_TEST);
+  mySetLight();
+  glEnable(GL_LIGHTING);
+  glEnable(GL_DEPTH_TEST);
 
-	int threshold = 100;
-	int	marker_num;
-	ARMarkerInfo* marker_info;
-	arDetectMarker(image, threshold, &marker_info, &marker_num);
+  int threshold = 100;
+  int markerNum;
+  ARMarkerInfo* markerInfo;
+  arDetectMarker(image, threshold, &markerInfo, &markerNum);
 
-	int k = -1;
-	for (int j = 0; j < marker_num; j++) {
-		if (marker_info[j].id == marker_id) {
-			if (k == -1) k = j;
-			else if (marker_info[j].cf > marker_info[k].cf) k = j;
-		}
-	}
 
-	if (k != -1) {
-		double center[2] = { 0, 0 };
-		double transMat[3][4];
-		double M[16];
-		arGetTransMat(&(marker_info[k]), center, marker_width, transMat);
-		arglCameraViewRH(transMat, M, 1.0);
+  for (int j = 0; j < markerNum; j++) {
+    glPushMatrix();
+    if (markerInfo[j].id == crossMarkerId) {
+      double center[2] = { 0, 0 };
+      double transMat[3][4];
+      double M[16];
+      arGetTransMat(&(markerInfo[j]), center, crossMarkerWidth, transMat);
+      arglCameraViewRH(transMat, M, 1.0);
 
-		glLoadMatrixd(M);
-		glTranslatef(0, 0, 40);
-		glutSolidCube(80);
-	}
+      glLoadMatrixd(M);
+      glTranslatef(0, 0, 40);
+      glutSolidCube(80);
+    }else if(markerInfo[j].id == pMarkerId){
+      double center[2] = { 0, 0 };
+      double transMat[3][4];
+      double M[16];
+      arGetTransMat(&(markerInfo[j]), center, pMarkerWidth, transMat);
+      arglCameraViewRH(transMat, M, 1.0);
 
-	glDisable(GL_LIGHTING);
-	glDisable(GL_DEPTH_TEST);
+      glLoadMatrixd(M);
+      glTranslatef(0, 0, 40);
+      glutSolidSphere(40, 100, 100);
+    }
+    glPopMatrix();
+  }
 
-	glutSwapBuffers();
+  glDisable(GL_LIGHTING);
+  glDisable(GL_DEPTH_TEST);
+
+  glutSwapBuffers();
 }
 
 void keyboard(unsigned char key, int x, int y) {
-	if (key == 27) {
-		exit(0);
-	}
+  if (key == 27) {
+    exit(0);
+  }
 }
 
 void quit() {
-	arVideoCapStop();
-	arVideoClose();
-	arglCleanup(arSettings);
+  arVideoCapStop();
+  arVideoClose();
+  arglCleanup(arSettings);
 }
 
 int main(int argc, char *argv[]) {
-	glutInit(&argc, argv);
-	glutInitWindowSize(width, height);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
-	glutCreateWindow("ARToolKit Sample");
-	arSettings = arglSetupForCurrentContext();
+  glutInit(&argc, argv);
+  glutInitWindowSize(width, height);
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+  glutCreateWindow("ARToolKit Sample");
+  arSettings = arglSetupForCurrentContext();
 
-	setupCamera("WDM_camera_flipV.xml", "camera_para.dat");
+  setupCamera("WDM_camera_flipV.xml", "camera_para.dat");
 
-	marker_id = arLoadPatt("patt.hiro");
+  crossMarkerId = arLoadPatt("cross.pat");
+  pMarkerId = arLoadPatt("p.pat");
 
-	glutDisplayFunc(display);
-	glutIdleFunc(display);
-	glutKeyboardFunc(keyboard);
-	atexit(quit);
+  glutDisplayFunc(display);
+  glutIdleFunc(display);
+  glutKeyboardFunc(keyboard);
+  atexit(quit);
 
-	glutMainLoop();
+  glutMainLoop();
 }
