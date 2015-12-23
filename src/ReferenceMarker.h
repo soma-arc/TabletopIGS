b7a4b98ef9cb905d10b961ba0cecbd2c31afbdbc
soma_arc <somaarcr@gmail.com>
2015-12-23 18:26:05 +0900

    Add ReferenceMarker

---
+++ src/ReferenceMarker.h
@@ -0,0 +1,17 @@
+#include <AR/ar.h>
+#include <string>
+#include <vector>
+
+class ReferenceMarker{
+ public:
+  ReferenceMarker(const char* markerPath, double markerWidth);
+  void getTransMat(ARMarkerInfo *info);
+  std::vector<double> getDistance(double otherMat[3][4]);
+  std::vector<double> getRotation(double otherMat[3][4]);
+
+  int patternId;
+  double markerWidth;
+  double center[2];
+  double transMat[3][4];
+  bool visible;
+};
