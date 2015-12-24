2059e85ef62cc78899c963119c19b052c80f6a9c
soma_arc <somaarcr@gmail.com>
2015-12-24 14:02:44 +0900

    Store distance and rotation in MarkedPoint

--- src/MarkedPoint.cpp
+++ src/MarkedPoint.cpp
@@ -1,5 +1,7 @@
 #include "MarkedPoint.h"
 
+MarkedPoint::MarkedPoint(){}
+
 MarkedPoint::MarkedPoint(const char* markerPath, double markerWidth){
   this->patternId = arLoadPatt(markerPath);
   this->markerWidth = markerWidth;
@@ -12,4 +14,14 @@ MarkedPoint::MarkedPoint(const char* markerPath, double markerWidth){
 void MarkedPoint::draw(){
   if(visible == false) return;
   fillCircle(DISPLAY_RADIUS, x.re(), y.re());
+}
+
+void MarkedPoint::setDistanceFromOrigin(ReferenceMarker origin){
+  distanceFromOrigin = origin.getDistance(this->transMat);
+  x = distanceFromOrigin[0];
+  y = distanceFromOrigin[1];
+}
+
+void MarkedPoint::setRotationFromOrigin(ReferenceMarker origin){
+  rotationFromOrigin = origin.getRotation(this->transMat);
 }
--- src/MarkedPoint.h
+++ src/MarkedPoint.h
@@ -3,11 +3,20 @@
 
 #include "Point.h"
 #include "ReferenceMarker.h"
+#include <vector>
+
+using namespace std;
 
 class MarkedPoint : public Point, public ReferenceMarker{
  public:
+  MarkedPoint();
   MarkedPoint(const char* markerPath, double markerWidth);
   virtual void draw();
+  void setDistanceFromOrigin(ReferenceMarker origin);
+  void setRotationFromOrigin(ReferenceMarker origin);
+
+  vector<double> distanceFromOrigin;
+  vector<double> rotationFromOrigin;
 };
 
 #endif
