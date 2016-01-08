2f78e0696c52eb8caac4f3b5f9bad8101b957cc7
soma_arc <somaarcr@gmail.com>
2016-01-08 15:33:21 +0900

    Modularize some code

--- src/main.cpp
+++ src/main.cpp
@@ -25,8 +25,9 @@
 
 ARParam arCameraParam;
 ARGL_CONTEXT_SETTINGS_REF arSettings;
-int width = 800;
-int height = 600;
+const int width = 800;
+const int height = 600;
+const int threshold = 100;
 int crossMarkerId;
 int pMarkerId;
 ReferenceMarker origin("resources/origin.pat", 40.0);
@@ -62,34 +63,6 @@ bool setupCamera(char videoConfigFile[], char cameraParamFile[]) {
   arInitCparam(&arCameraParam);
 
   return true;
-}
-
-void cylinder(float radius, float height, int sides){
-  double pi = 3.1415;
-  glNormal3d(0.0, 1.0, 0.0);
-  glBegin(GL_POLYGON);
-  for(double i = 0; i < sides; i++) {
-    double t = pi*2/sides * (double)i;
-    glVertex3d(radius * cos(t), radius * sin(t), height);
-  }
-  glEnd();
-
-  glBegin(GL_QUAD_STRIP);
-  for(double i=0;i<=sides;i=i+1){
-    double t = i*2*pi/sides;
-    glNormal3f((GLfloat)cos(t), (GLfloat)sin(t), 0.0);
-    glVertex3f((GLfloat)(radius*cos(t)),(GLfloat)(radius*sin(t)), -height);
-    glVertex3f((GLfloat)(radius*cos(t)),(GLfloat)(radius*sin(t)), height);
-  }
-  glEnd();
-
-  glNormal3d(0.0, -1.0, 0.0);
-  glBegin(GL_POLYGON);
-  for(double i = sides; i >= 0; --i) {
-    double t = pi*2/sides * (double)i;
-    glVertex3d(radius * cos(t), radius * sin(t), -height);
-  }
-  glEnd();
 }
 
 void setGlColorHSVA(float h, float s, float v, float a){
@@ -149,6 +122,65 @@ void mySetLight() {
   glEnable(GL_LIGHT0);
 }
 
+void drawOrigin(){
+    glBegin(GL_LINES);
+  glVertex3d(-10., 0, 0.);
+  glVertex3d(10., 0, 0.);
+  glVertex3d(0, 10., 0.);
+  glVertex3d(0, -10., 0.);
+  glEnd();
+}
+
+vector<Line> getLines(vector<MarkedPoint> linePoints){
+  vector<Line> lines;
+  for_each(linePoints.begin(), linePoints.end(),
+           [&](MarkedPoint p){
+             vector<double> distance = p.distanceFromOrigin;
+             vector<double> rotation = p.rotationFromOrigin;
+             Point p1(distance[0], distance[1]);
+             Line l(p1,
+                    Point(p1.x.re() + 20 * cos(rotation[2] + M_PI_2),
+                          p1.y.re() + 20 * sin(rotation[2] + M_PI_2)));
+             setGlColorHSVA(0., 0., 0., 1.);
+             l.z = distance[2];
+             lines.push_back(l);
+           });
+  return lines;
+}
+
+vector<Circle> getCircles(vector<MarkedPoint> circlePoints){
+  vector<Circle> circles;
+  float hue = 0.0;
+  for_each(circlePoints.begin(), circlePoints.end(),
+           [&](MarkedPoint p){
+             vector<double> distance = p.distanceFromOrigin;
+             vector<double> rotation = p.rotationFromOrigin;
+             Circle c(Point(distance[0], distance[1]), 30 + 15 * sin(rotation[2]));
+             c.z = distance[2];
+             c.color[0] = hue;
+             circles.push_back(c);
+             hue += 0.3;
+           });
+  return circles;
+}
+
+void drawInversedCircles(vector<Circle> circles){
+  for_each(circles.begin(),
+           circles.end(),
+           [&](Circle c){
+             for_each(circles.begin(),
+                      circles.end(),
+                      [&](Circle c2){
+                        if(c.center.x.re() == c2.center.x.re() &&
+                           c.center.y.re() == c2.center.y.re()) return;
+                        Circle nc = Mobius::circleInverse(c, c2);
+                        nc.z = c.z;
+                        setGlColorHSVA(c2.color[0], 1., 1., 1.);
+                        nc.fill();
+                      });
+           });
+}
+
 int circleId;
 int lineId;
 
@@ -169,34 +201,25 @@ void display() {
   glLoadIdentity();
 
   mySetLight();
-  // glEnable(GL_LIGHTING);
-  // glEnable(GL_DEPTH_TEST);
-  // glEnable(GL_COLOR_MATERIAL);
 
-  int threshold = 100;
   int markerNum;
   ARMarkerInfo* markerInfo;
   arDetectMarker(image, threshold, &markerInfo, &markerNum);
-  vector<MarkedPoint> points;
+  vector<MarkedPoint> circlePoints;
   vector<MarkedPoint> linePoints;
   for (int j = 0; j < markerNum; j++) {
     glPushMatrix();
     if (markerInfo[j].id == origin.patternId) {
       double M[16];
       origin.getTransMat(&(markerInfo[j]));
-      arglCameraViewRH(origin.transMat, M, 1.0);
-      glLoadMatrixd(M);
-      //      glTranslatef(0, 0, 20);
-      //      glutSolidCube(40);
     }else{
-      glPushMatrix();
       if(markerInfo[j].id == -1) continue;
       if(markerInfo[j].id == circleId){
         MarkedPoint p = idMarkedPointMap[markerInfo[j].id];
         p.getTransMat(&(markerInfo[j]));
         p.setDistanceFromOrigin(origin);
         p.setRotationFromOrigin(origin);
-        points.push_back(p);
+        circlePoints.push_back(p);
       }else{
         MarkedPoint p = idMarkedPointMap[markerInfo[j].id];
         p.getTransMat(&(markerInfo[j]));
@@ -204,7 +227,6 @@ void display() {
         p.setRotationFromOrigin(origin);
         linePoints.push_back(p);
       }
-      glPopMatrix();
     }
     glPopMatrix();
   }
@@ -212,72 +234,30 @@ void display() {
   double M[16];
   arglCameraViewRH(origin.transMat, M, 1.0);
   glLoadMatrixd(M);
+  drawOrigin();
 
-  vector<Line> lines;
   glLineWidth(5.f);
-  for_each(linePoints.begin(), linePoints.end(),
-           [&](MarkedPoint p){
-             vector<double> distance = p.distanceFromOrigin;
-             vector<double> rotation = p.rotationFromOrigin;
-             Point pp(distance[0], distance[1]);
-             Line l(pp,
-                    Point(pp.x.re() + 50 * cos(rotation[2] + M_PI_2),
-                          pp.y.re() + 50 * sin(rotation[2] + M_PI_2)));
-             lines.push_back(l);
-             setGlColorHSVA(0., 0., 0., 1.);
-             l.z = distance[2];
-             l.draw();
-           });
+  vector<Line> lines = getLines(linePoints);
+  for_each(lines.begin(), lines.end(),
+           [](Line l){ l.draw();});
 
-  vector<Circle> circles;
-  float hue = 0.0;
-  for_each(points.begin(), points.end(),
-           [&](MarkedPoint p){
-             vector<double> distance = p.distanceFromOrigin;
-             vector<double> rotation = p.rotationFromOrigin;
-             Circle c(Point(distance[0], distance[1]), 30 + 15 * sin(rotation[2]));
-             c.z = distance[2];
-             c.color[0] = hue;
-             circles.push_back(c);
-             setGlColorHSVA(hue, 1., 1., 1.);
-             hue += 0.3;
+  vector<Circle> circles = getCircles(circlePoints);
+  for_each(circles.begin(), circles.end(),
+           [&](Circle c){
+             setGlColorHSVA(c.color[0], 1., 1., 1.);
              c.fill();
              for_each(lines.begin(), lines.end(),
                       [&](Line l){
-                        Circle cir = Mobius::circleInverse(c, l);
+                        Circle invCircle = Mobius::circleInverse(c, l);
                         setGlColorHSVA(1., 0., 0., 1.);
-                        cir.z = c.z;
-                        cir.draw();
+                        invCircle.z = c.z;
+                        invCircle.draw();
                       });
            });
 
-  const int maxLevel = 1;
-  vector<vector<Circle>> inversedCircles;
-  inversedCircles.push_back(circles);
-  for(int i = 0; i < maxLevel ; i++){
-    vector<Circle> v;
-    inversedCircles.push_back(v);
-    for_each(inversedCircles[i].begin(),
-             inversedCircles[i].end(),
-             [&](Circle c){
-               for_each(inversedCircles[i].begin(),
-                        inversedCircles[i].end(),
-                        [&](Circle c2){
-                          if(c.center.x.re() == c2.center.x.re() &&
-                             c.center.y.re() == c2.center.y.re()) return;
-                          Circle in = Mobius::circleInverse(c, c2);
-                          in.z = c.z;
-                          setGlColorHSVA(c2.color[0], 1., 1., 1.);
-                          //setGlColorHSVA(0.15 * (i + 1), 1., 1., 1.);
-                          in.fill();
-                          inversedCircles[i + 1].push_back(in);
-                        });
-             });
-  }
+  drawInversedCircles(circles);
+
   glPopMatrix();
-  // glDisable(GL_LIGHTING);
-  // glDisable(GL_DEPTH_TEST);
-  // glDisable(GL_COLOR_MATERIAL);
   glutSwapBuffers();
 }
 
@@ -294,23 +274,13 @@ void quit() {
 }
 
 void loadPatterns(){
-  //  MarkedPoint pointA("resources/A.pat", 40);
-  //  MarkedPoint pointB("resources/B.pat", 40);
   MarkedPoint pointC("resources/C.pat", 40);
   MarkedPoint pointD("resources/D.pat", 40);
-  //  MarkedPoint pointE("resources/E.pat", 40);
-  //  MarkedPoint pointF("resources/F.pat", 40);
-  //  MarkedPoint pointP("resources/P.pat", 40);
   MarkedPoint rect("resources/rect.pat", 40);
   MarkedPoint line("resources/line.pat", 40);
   MarkedPoint line2("resources/line2.pat", 40);
-  //  idMarkedPointMap[pointA.patternId] = pointA;
-  //idMarkedPointMap[pointB.patternId] = pointB;
   idMarkedPointMap[pointC.patternId] = pointC;
   idMarkedPointMap[pointD.patternId] = pointD;
-  //  idMarkedPointMap[pointE.patternId] = pointE;
-  //  idMarkedPointMap[pointF.patternId] = pointF;
-  //  idMarkedPointMap[pointP.patternId] = pointP;
   idMarkedPointMap[rect.patternId] = rect;
   idMarkedPointMap[line.patternId] = line;
   idMarkedPointMap[line2.patternId] = line2;
